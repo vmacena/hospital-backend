bdaa117d9297b0d0bcaf34ecee30ef662e21227e
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-12 17:51:02 -0300

    feat: add CORS middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, {Request, Response, NextFunction} from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import router from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
