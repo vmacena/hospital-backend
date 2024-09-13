11286ec2f04ac37cb85e79a67923bf078cf64345
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-13 15:14:30 -0300

    refactor: make error message for existing admin more user-friendly

--- src/services/CreateAdminService.ts
+++ src/services/CreateAdminService.ts
@@ -4,6 +4,16 @@ class CreateAdminService {
   private prisma = new PrismaClient();
 
   async execute(record: string) {
+    const existingAdmin = await this.prisma.admin.findUnique({
+      where: {
+        record,
+      },
+    });
+
+    if (existingAdmin) {
+      throw new Error("Admin already registered with this credential.");
+    }
+
     const admin = await this.prisma.admin.create({
       data: {
         record,
