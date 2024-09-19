2ba500f18894291d2731655e93793d3f24fbdebe
gabrmanh <gmanhani@yahoo.com>
2024-09-18 22:07:38 -0300

    feat: set routes for patient exams and appointments

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,8 @@ import { GetPatientDataController } from "./controllers/patient/GetPatientDataCo
 import { ScheduleExamController } from "./controllers/exam/ScheduleExamController";
 import { GetAllLogsController } from "./controllers/log/GetAllLogsController";
 import { FindDoctorAppointmentsAndExamsController } from "./controllers/doctor/FindDoctorAppointmentsAndExamsController"; // Importe o controlador
+import { PatientAppointmentController } from "./controllers/patient/PatientAppointmentController";
+import { PatientExamController } from "./controllers/patient/PatientExamController";
 import { authenticateToken } from "./middlewares/authenticateToken";
 import { checkAdminAccess } from "./middlewares/checkAdminAccess"; // Importe o middleware
 
@@ -26,22 +28,36 @@ const findAllPatientsController = new FindAllPatientsController();
 const scheduleExamController = new ScheduleExamController();
 const getAllLogsController = new GetAllLogsController();
 const findDoctorAppointmentsAndExamsController = new FindDoctorAppointmentsAndExamsController(); // Instancie o controlador
+const patientAppointmentController = new PatientAppointmentController();
+const patientExamController = new PatientExamController();
 
 router.post("/admin/register", createAdminController.handle);
 router.post("/admin/login", loginAdminController.handle);
+
+router.post("/doctor/register", createDoctorController.handle);
+router.post("/doctor/login", loginDoctorController.handle);
+router.get("/doctor/patients", authenticateToken, findAllPatientsController.handle);
+router.get("/doctor/appointments-exams", authenticateToken, findDoctorAppointmentsAndExamsController.handle); // Adicione a nova rota
 
 router.post("/patient/register", createPatientController.handle);
 router.post("/patient/login", loginPatientController.handle);
 router.get("/patient/:id", authenticateToken, getPatientDataController.handle);
 
-router.post("/doctor/register", createDoctorController.handle);
-router.post("/doctor/login", loginDoctorController.handle);
-router.get("/doctor/patients", findAllPatientsController.handle);
+router.post("/patient/appointments/", authenticateToken, patientAppointmentController.create);
+router.get("/patient/appointments/", authenticateToken, patientAppointmentController.findAll);
+router.put("/patient/appointments/update-date", authenticateToken, patientAppointmentController.updateDate);
+router.delete("/patient/appointments/cancel", authenticateToken, patientAppointmentController.cancel);
+router.put("/patient/appointments/finish", authenticateToken, patientAppointmentController.finish);
 
-router.post("/exam/schedule", authenticateToken, scheduleExamController.handle);
+router.post("/patient/exams/", authenticateToken, patientExamController.create);
+router.get("/patient/exams/", authenticateToken, patientExamController.findAll);
+router.put("/patient/exams/update-date", authenticateToken, patientExamController.updateDate);
+router.delete("/patient/exams/cancel", authenticateToken, patientExamController.cancel);
+router.put("/patient/exams/give-result", authenticateToken, patientExamController.giveResult);
+
+router.post("/exam/schedule", authenticateToken, scheduleExamController.handle); // sorry :(
 
 router.get("/logs", authenticateToken, checkAdminAccess, getAllLogsController.handle); // Adicione o middleware aqui
 
-router.get("/doctor/appointments-exams", authenticateToken, findDoctorAppointmentsAndExamsController.handle); // Adicione a nova rota
 
 export { router };
