93b1a846aded8d07a420d12a3be99c6a9d3cbf89
gabrmanh <gmanhani@yahoo.com>
2024-11-06 20:29:47 -0300

    feat: added address to patient controller

--- src/controllers/patient/CreatePatientController.ts
+++ src/controllers/patient/CreatePatientController.ts
@@ -1,20 +1,30 @@
 import { Request, Response } from "express";
 import { CreatePatientService } from "../../services/patient/CreatePatientService";
+import { Address } from "../../services/address/Address"; 
+
+interface CreatePatientRequestBody {
+  namePatient: string;
+  email: string;
+  address: Address;
+}
 
 class CreatePatientController {
-  async handle(req: Request, res: Response) {
-    const { namePatient, email } = req.body;
+  async handle(req: Request<{}, {}, CreatePatientRequestBody>, res: Response) {
+    const { namePatient, email, address } = req.body;
     const accessLevelId = 3;
 
     const createPatientService = new CreatePatientService();
-    const patient = await createPatientService.execute(namePatient, email, accessLevelId);
 
-    const responsePatient = {
-      ...patient,
-      susNumber: patient.susNumber.toString(),
-    };
-
-    return res.json(responsePatient);
+    try {
+      const patient = await createPatientService.execute(namePatient, email, accessLevelId, address);
+      const responsePatient = {
+        ...patient,
+        susNumber: patient.susNumber.toString(),
+      };
+      return res.json(responsePatient);
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
   }
 }
 
--- src/services/patient/CreatePatientService.ts
+++ src/services/patient/CreatePatientService.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { Address } from "../address/Address";
 
 class CreatePatientService {
   private prisma = new PrismaClient();
 
-  async execute(namePatient: string, email: string, accessLevelId: number) {
+  async execute(namePatient: string, email: string, accessLevelId: number, address: Address) {
     const existingPatient = await this.prisma.patient.findUnique({
       where: {
         email,
@@ -16,12 +17,26 @@ class CreatePatientService {
 
     const susNumber = BigInt(Date.now() + Math.floor(Math.random() * 1000));
 
+    const createdAddress = await this.prisma.address.create({
+      data: {
+        cep: address.cep,
+        street: address.street,
+        district: address.district,
+        state: address.state,
+        city: address.city,
+        number: address.number,
+        complement: address.complement,
+      },
+    });
+
+    // Create the patient with the addressId
     const patient = await this.prisma.patient.create({
       data: {
         susNumber,
         namePatient,
         email,
         accessLevelId,
+        addressId: createdAddress.id,
       },
     });
 
