b7cbc4ab178fa94f27b5412f3eee9aba8768dc55
vmacena <macena.v@aluno.ifsp.edu.br>
2024-11-08 02:04:19 -0300

    refactor: converting the string to a decimal number base 10

--- src/services/patient/CreatePatientService.ts
+++ src/services/patient/CreatePatientService.ts
@@ -4,7 +4,7 @@ import { Address } from "../address/Address";
 class CreatePatientService {
   private prisma = new PrismaClient();
 
-  async execute(namePatient: string, email: string, accessLevelId: number, address: Address, picture_url: string) {
+  async execute(namePatient: string, email: string, address: Address, picture_url: string, accessLevelId: number = 1) {
     const existingPatient = await this.prisma.patient.findUnique({
       where: {
         email,
@@ -17,6 +17,21 @@ class CreatePatientService {
 
     const susNumber = BigInt(Date.now() + Math.floor(Math.random() * 1000));
 
+    console.log("Address object:", address);
+    console.log("Address fields:", {
+      cep: address.cep,
+      street: address.street,
+      district: address.district,
+      state: address.state,
+      city: address.city,
+      number: address.number,
+      complement: address.complement,
+    });
+
+    if (!address.cep || !address.street || !address.district || !address.state || !address.city || !address.number) {
+      throw new Error("All address fields are required.");
+    }
+
     const createdAddress = await this.prisma.address.create({
       data: {
         cep: address.cep,
@@ -24,7 +39,7 @@ class CreatePatientService {
         district: address.district,
         state: address.state,
         city: address.city,
-        number: address.number,
+        number: parseInt(address.number, 10),
         complement: address.complement,
       },
     });
@@ -36,7 +51,7 @@ class CreatePatientService {
         email,
         accessLevelId,
         addressId: createdAddress.id,
-        picture_url
+        picture_url,
       },
     });
 
